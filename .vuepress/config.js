ccea96f5301943aaaf5b26288d3361a84c8f5ec6
incide-tecnico <tecnico@incide.es>
2020-03-05 14:48:26 +0100

    Fix vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,64 @@
+//export default {
+module.exports = {
+    base: '/rvt2-docs/',
+    title: 'Revealer Toolkit 2',
+    description: 'An open source platform for forensics investigations',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'RVT2', link: '/rvt2/' },
+            { text: 'RVT2-analyzer', link: '/analyzer/' }
+        ],
+        sidebar: [
+            {
+                title: 'RVT2',
+                children: [
+                    '/rvt2/',
+                    '/rvt2/install',
+                    '/rvt2/definitions',
+                    '/rvt2/configure',
+                    '/rvt2/folder',
+                    '/rvt2/base',
+                    '/rvt2/common',
+                    '/rvt2/windows',
+                    '/rvt2/macos',
+                    '/rvt2/linux',
+                    '/rvt2/ios',
+                    '/rvt2/indexer',
+                    '/rvt2/extend',
+                    '/rvt2/examples'
+                ]
+            },
+            {
+                title: 'RVT2 analyzer',
+                children: [
+                    '/analyzer/',
+                    '/analyzer/install',
+                    '/analyzer/development'
+                ]
+            },
+            'contact'
+        ],
+        // Configure "edit this page"
+        repo: 'http://gitlab.incide.es/incide-repos/rvt2-docs',
+        repoLabel: 'Contribute!',
+        editLinks: true,
+        editLinkText: 'Help us to improve this page!',
+        lastUpdated: 'Last Updated', // string | boolean
+        // configure the languages
+        locales: {
+            '/es/': {
+                editLinkText: 'Ayúdanos a mejorar esta página.'
+            }
+        }
+    },
+    locales: {
+        '/': {
+            lang: 'en', // this will be set as the lang attribute on <html>
+        },
+        '/es/': {
+            lang: 'es',
+            description: 'Una plataforma para análisis forense basada en software libre.',
+        }
+    }
+}
